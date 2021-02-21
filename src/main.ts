0236e91bb6bb3e9bcb610aa071cf47da494df659
ShareManT <sharemant@163.com>
2021-02-21 23:26:49 +0800

    chore: add windiCss(tailwindCss) plugin and set the configurations

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Utils from './plugins/utils'
 import Router from './router'
 import Store from './store'
+import 'windi.css'
 
 const app = createApp(App)
 
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite-plugin-windicss'
+
+export default defineConfig({
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+            },
+        },
+    },
+})
