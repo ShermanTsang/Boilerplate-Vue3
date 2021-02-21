7261a6965c9aefbfb77cf886d9e832e38cb412fa
ShareManT <sharemant@163.com>
2021-02-21 20:17:23 +0800

    chore: set vuex with initial configurations

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Utils from './plugins/utils'
 import Router from './router'
+import Store from './store'
 
 const app = createApp(App)
 
 app
     .use(Router)
+    .use(Store)
     .use(Utils)
     .mount('#app')
