5d5680705062ca978b32e4d3d37cfc3effdbdada
ShareManT <sharemant@163.com>
2021-03-10 00:57:51 +0800

    feat(plugin): add axios and configurations

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,15 +3,25 @@
 
 <template>
   <div class="p-2">Welcome to Index page!</div>
+  {{ array }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, getCurrentInstance, reactive, onMounted, toRefs } from 'vue';
 
 export default defineComponent({
   name: 'Index',
   setup() {
-    return {};
+    const { ctx } = getCurrentInstance();
+    const state = reactive({
+      text: 'vue',
+      array: {},
+    });
+    onMounted(async () => {
+      const res = await ctx.$api.get('/common/timeline', { name: 1 });
+      state.array = res.data;
+    });
+    return { ...toRefs(state) };
   },
 });
 </script>
