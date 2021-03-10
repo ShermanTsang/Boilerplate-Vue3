20a7f8a2b50237bfe572c19c150be193115387df
ShareManT <sharemant@163.com>
2021-03-11 01:08:23 +0800

    chore: set eslint + prettier + typescript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,32 @@
+/** @format */
+
 module.exports = {
-  extends: ['plugin:vue/vue3-essential', 'airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['vue'],
-  rules: {
-    // 自己写一些想配置的规则
-  },
-  overrides: [
-    {
-      files: ['*.vue'],
-      rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'generator-star-spacing': 'off',
-        'vue/no-v-html': 'off',
-        quotes: [1, 'single'],
-        'object-curly-newline': 'off',
-        'no-void': 'off',
-        camelcase: 'off',
-        'vue/comment-directive': 'off',
-        'vue/html-self-closing': ['error', {
-          html: {
-            void: 'never',
-            normal: 'any',
-            component: 'any',
-          },
-          svg: 'always',
-          math: 'always',
-        }],
-      },
+    parser: 'vue-eslint-parser' /* 指定如何解析语法*/,
+    extends: ['plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    /* 优先级低于parse的语法解析配置 */
+    parserOptions: {
+        parser: '@typescript-eslint/parser' /* 解析ts语法 */,
+        ecmaVersion: 2020,
+        sourceType: 'module',
     },
-  ],
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [
-          ['/@', './src'],
-        ],
-        extensions: ['.ts', '.js', '.jsx', '.json', '.vue'],
-      },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {},
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {},
+        },
+    ],
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [['/@', './src']],
+                extensions: ['.ts', '.js', '.jsx', '.json', '.vue'],
+            },
+        },
     },
-  },
-};
+    env: {
+        browser: true,
+        node: true,
+    },
+}
