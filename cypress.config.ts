facacb8e8e8c009fd9da7f4a9c47dd34e323a728
ShermanT <sharemant@163.com>
2023-04-10 23:49:41 +0800

    feat: replace the whole vitesse boilerplate with pure latest official vue scaffold

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'cypress'
-import vitePreprocessor from 'cypress-vite'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3333',
-    chromeWebSecurity: false,
-    specPattern: 'cypress/e2e/**/*.spec.*',
-    supportFile: false,
-    setupNodeEvents(on) {
-      on('file:preprocessor', vitePreprocessor())
-    },
-  },
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:4173'
+  }
 })
